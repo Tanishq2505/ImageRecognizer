d43fea61235893d1576bcddedba8c8a2065f25d9
R3DKNIGHT <night.r3d29.6.2000@gmail.com>
2021-02-03 20:18:21 +0530

    works sometime, not always, needs image processing

---
+++ autoReloadValorant.py
@@ -0,0 +1,21 @@
+import pyautogui
+import pytesseract
+import cv2
+import keyboard
+import time
+import numpy as np
+pytesseract.pytesseract.tesseract_cmd = r'D:\Programs\pytesseract\tesseract.exe'
+
+
+# img = pyautogui.screenshot(region=(1277,1003,52,43))
+# img.save(r'D:\savedimg.png')
+while keyboard.is_pressed('q') == False:
+    if pyautogui.pixel(1280,1045)[0] == 123 and pyautogui.pixel(1280,1045)[1] == 48 and pyautogui.pixel(1280,1045)[2] == 44:
+        if keyboard.is_pressed('1') == True:
+            time.sleep(2.6)
+        else:    
+            time.sleep(0.6)
+            keyboard.press_and_release('2')
+
+
+# pyautogui.locateOnScreen()
